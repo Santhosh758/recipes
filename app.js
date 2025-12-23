b05bdea4d099c0867235226cbee581452a7d2b67
Santhosh kumar <santhoshkrishnan18052005@gmail.com>
2025-12-23 19:43:24 +0530

    new

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const recipeRoutes = require('./routes/recipeRoutes');
+require('dotenv').config(); // Ithu romba mukkiyam!
 
-dotenv.config();
-const app = express();
+// process.env moolama Render-la neenga kudutha 'MONGODB_URI' edukkum
+const dbURI = process.env.MONGODB_URI;
 
-// Middleware to parse JSON
-app.use(express.json());
-
-// Routes Middleware
-app.use('/api/recipes', recipeRoutes);
-
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/recipesDB')
-    .then(() => console.log('Connected to MongoDB... ✅'))
-    .catch(err => console.error('Could not connect to MongoDB... ❌', err));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+mongoose.connect(dbURI)
+  .then(() => console.log("Recipes Database Connected Successfully!"))
+  .catch((err) => console.log("Connection Error: ", err));
